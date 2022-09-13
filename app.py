4547005f46cb52e4aaa654fc1eedbd0f903c4621
kallasrikanth1999 <kalla.srikanth010@gmail.com>
2022-09-13 14:38:52 +0530

    t
    de

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask,render_template,url_for,request,redirect
+import numpy as np
+import pandas as pd
+import joblib
+import pickle
+
+app=Flask(__name__)
+
+model=joblib.load('decmodel.pkl')
+
+
+@app.route('/')
+@app.route('/main')
+def main():
+	return render_template('main.html')
+
+@app.route('/predict',methods=['post'])
+def predict():
+	int_features=[[x for x in request.form.values()]]
+	
+	c=['N','P','K','temperature','humidity','ph','rainfall']
+    
+	fin=pd.DataFrame(int_features,columns=c)
+	result=model.predict(fin)
+	return render_template("main.html",prediction_text=" The recommende crop is : {}".format(result))
+	
+
+
+
+
+if __name__ =="__main__":
+	app.debug=True
+	app.run(host = '127.0.0.1',port= 7000)
